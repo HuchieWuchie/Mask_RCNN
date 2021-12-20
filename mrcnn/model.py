07d8d7fd5d31e89c14d8ab6da24a25eefaea45c2
Albert Christensen <alchri17@student.aau.dk>
2021-12-20 17:19:29 +0100

    sigmoid

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -1199,36 +1199,18 @@ def mrcnn_mask_loss_graph(target_masks, target_class_ids, pred_masks):
                 with values from 0 to 1.
     """
     # Reshape for simplicity. Merge first two dimensions into one.
-    #print("target_masks: ", target_masks, "      pred_masks: ", pred_masks)
-    target_class_ids = K.reshape(target_class_ids, (-1,))
     mask_shape = tf.shape(target_masks)
     target_masks = K.reshape(target_masks, (-1, mask_shape[3], mask_shape[4]))
-    #target_masks = K.reshape(target_masks, (-1, mask_shape[2], mask_shape[3]))
+    
     pred_shape = tf.shape(pred_masks)
     pred_masks = K.reshape(pred_masks,
                            (-1, pred_shape[3], pred_shape[4]))
-    #print("RESHAPED: target_masks: ", target_masks, "      pred_masks: ", pred_masks)
-    # Permute predicted masks to [N, num_classes, height, width]
-    #pred_masks = tf.transpose(pred_masks, [0, 3, 1, 2])
-    #print("TRANSPOSED: target_masks: ", target_masks, "      pred_masks: ", pred_masks)
 
-    # Only positive ROIs contribute to the loss. And only
-    # the class specific mask of each ROI.
-    #positive_ix = tf.where(target_class_ids > 0)[:, 0]
-    #positive_class_ids = tf.cast(
-    #    tf.gather(target_class_ids, positive_ix), tf.int64)
-    #indices = tf.stack([positive_ix, positive_class_ids], axis=1)
-    #print("indices: ", indices)
-
-    # Gather the masks (predicted and true) that contribute to loss
     y_true = target_masks
-    #y_pred = tf.gather_nd(pred_masks, indices)
     y_pred = pred_masks
 
-    #print("y_true: ", y_true, "   y_pred: ", y_pred)
-
     # Compute binary cross entropy. If no positive ROIs, then return 0.
-    # shape: [batch, roi, num_classes]
+    # shape: [batch, roi, NUM_AFFORDANCES]
     loss = K.switch(tf.size(y_true) > 0,
                     K.binary_crossentropy(target=y_true, output=y_pred),
                     tf.constant(0.0))
